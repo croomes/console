efc0070649ebfd65b3f443ee9d9c6e78f866fdda
Simon Croome <simon@croome.org>
2013-06-23 00:55:40 +0100

    Initial commit, vigin rails4-rc2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Console::Application.config.secret_key_base = '3b043bda6e04dd0f41aacb3c11cc7708eaae042d76ee38128ca8938ea5f22eac4063aeea011c2a9419dd3574ca0be914d4c6298d733028d8a8cf03d9e4178121'
